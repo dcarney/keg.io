970b8a17c4a1cd18cbce430f106f788b236513f7
C4 <c4krauss@gmail.com>
2012-03-10 12:40:20 -0800

    Adding V2 Arduino code and libraries

---
+++ arduino/v2/Credentials.h
@@ -0,0 +1,13 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+char passphrase[] = "xxx";
+char ssid[] = "xxx";
+char clientId[] = "1111";
+uint8_t clientSecret[]={
+  // s3cr3t
+  0x73,0x33,0x63,0x72,0x33,0x74
+};
+int clientSecretLength = 6;
+
+#endif
