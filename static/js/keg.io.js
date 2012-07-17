24389e340ccfff75566d7fe70205758e7618c90e
Dylan Carney <dcarney@zulily.com>
2012-07-17 11:47:04 -0700

    WIP: Adds functionality to switch kegerators on dropdown selection

--- static/js/keg.io.js
+++ static/js/keg.io.js
@@ -1,10 +1,24 @@
-var switchKegerator = function(kegeratorId) {
-    $.getJSON("/kegerators/" + selectedId, function(data) {
+ var socketDebug = function(msg, data) {
+    console.log("socket event: '" + msg + "' data: " + (data === null ? "" : JSON.stringify(data)));
+};
 
+
+var switchKegerator = function(kegeratorId) {
+    $.getJSON("/kegerators/" + kegeratorId, function(data) {
+         console.log(data);
+         var kegerator = data;
+         if (_.isArray(data)) {
+            kegerator = data[0];
+         }
+         $('#kegerator_details').empty();
+         $('#kegerator_details').append("<li class='nav-header'>" + kegerator.name + "</li>");
+         $('#kegerator_details').append("<li class='nav-header'>" + kegerator.description + "</li>");
     });
-}
+};
 
 $(document).ready(function(){
+
+    // Get the list of available kegerators, populate the dropdown with them
     $.getJSON("/kegerators", function(kegerators) {
         console.log(kegerators);
         console.log(_.pluck(kegerators, 'kegerator_id'));
@@ -17,16 +31,13 @@ $(document).ready(function(){
         //console.log(drinker);
 
         _.each(ids, function(id) {
-         //   $('.dropdown-menu').append("<li><a href='#'>" + id + "</a></li>");
+            $('.dropdown-menu').append("<li><a href='#'>" + id + "</a></li>");
         });
     });
 });
 
 $('.dropdown-menu').on('click', 'li', function(event){
-    var selectedId = $(event.srcElement).html()
-    console.log(selectedId);
-
-    $.getJSON("/kegerators/" + selectedId, function(data) {
-        console.log(data);
-    });
+    var selectedId = $(event.srcElement).html();
+    console.log('New kegerator selected: ' + selectedId);
+    switchKegerator(selectedId);
 });
