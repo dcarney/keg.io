1ee383594b68de61ce649528428c93a024d94c61
Dylan Carney <dcarney@zulily.com>
2012-11-16 18:25:53 -0800

    Forces arduino to reconnect to keg.io on a broken TCP connection
      - New #defines control the intervals for connection health
        and reconnection frequency
      - Removes unused code to free up valuable(!) memory
      - Status LED now reflects HTTP connection status to keg.io

--- arduino/kegio/kegio.h
+++ arduino/kegio/kegio.h
@@ -7,12 +7,27 @@
   char kegioSuffix[] = ":::";
   #define KEGIO_SUFFIX_LENGTH 3
 
+  // How long can we go without an HTTP 200 before re-initializing the
+  // wifly http client? (and thus the TCP connection)
+  //
+  // Obviously, this value is tied to how often you're sending regular updates
+  // to the server (mostly likely with temperatures); see below...
+  #define WIFLY_RESET_INTERVAL_MS 21000
+
+  // How often should we attempt a reset of the wifly http client if the
+  // above interval is exceeded?  This should be frequent enough to be
+  // responsive, but not so frequent that a successful reconnect isn't thwarted
+  // by an another reconnection attempt. Between 5s and 10s seems to work well.
+  #define WIFLY_RESET_ATTEMPT_INTERVAL_MS 9000
+
   // How often should the arduino check the temperature and report to keg.io?
-  #define TEMPERATURE_SEND_INTERVAL_MS 30000
+  // This interval not only serves to send temperature info to the server, but
+  // also as a "status heartbeat"
+  #define TEMPERATURE_SEND_INTERVAL_MS 10000
 
   // How long should the solenoid stay open per scan?  This depends on the flow
   // rate that your particular setup is providing, and personal perference
-  #define SOLENOID_OPEN_DURATION_MS 15000
+  #define SOLENOID_OPEN_DURATION_MS 18000
 
   // correction factor to compensate for various
   // flow meter setups/calibrations
